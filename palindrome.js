887af593ea49c922f393df81d86409e6414451e7
荒川健 <arakawaken@arakawakennoMacBook-Air.local>
2023-08-23 11:45:00 +0900

    Prevent Jekyll build

---
+++ palindrome.js
@@ -0,0 +1,23 @@
+// reverse()メソッドを定義して全てのStringで使えるようにする
+String.prototype.reverse = function() {
+    return Array.from(this).reverse().join("");
+}
+
+String.prototype.blank = function() {
+    return !!this.match(/^\s+$/);
+}
+
+// Phraseオブジェクトを定義する
+function Phrase(content) {
+    this.content = content;
+
+    // パリンドロームのテスト用に変換したcontentを返す
+    this.processedContent = function processedContent() {
+        return this.content.toLowerCase();
+    }
+
+    // パリンドロームならtrueを、違うならfalseを返す
+    this.palindrome = function palindrome() {
+        return this.processedContent() === this.processedContent().reverse();
+    }
+}
