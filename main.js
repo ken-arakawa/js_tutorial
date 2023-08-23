2c3774210b3e10006e62ee17109cafecdcba5131
荒川健 <arakawaken@arakawakennoMacBook-Air.local>
2023-08-23 14:08:53 +0900

    Finish Chapter9

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 let Phrase = require("ken-palindrome");
 
-let string = prompt("パリンドロームをテストしたい文字列を入力してください：");
-let phrase = new Phrase(string);
+function palindromeTester(event) {
+    event.preventDefault();
 
-if  (phrase.palindrome()) {
-    alert(`"${phrase.content}"はパリンドロームです`);
-} else {
-    alert(`"${phrase.content}"はパリンドロームではありません`)
+    let phrase = new Phrase(event.target.phrase.value);
+    let palindromeResult = document.querySelector("#palindromeResult");
+
+    if  (phrase.palindrome()) {
+        palindromeResult.innerHTML = `"<strong>${phrase.content}</strong>"はパリンドロームです！`;
+    } else {
+        palindromeResult.innerHTML = `"${phrase.content}"はパリンドロームではありません`;
+    }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    let tester = document.querySelector("#palindromeTester");
+    tester.addEventListener("submit", function(event) {
+        palindromeTester(event);
+    });
+});
